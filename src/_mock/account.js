6cfa9ae9b4648b2c420defd2eef2ad70c2662164
Amir <amazingOmerkhan@gmail.com>
2024-07-21 17:10:45 +0500

    first commit

---
+++ src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Aidarkhan Amirkhan',
+  email: 'pchel@beelieve.org',
+  photoURL: '/assets/images/avatars/avatar_26.jpg',
+};
---
+++ src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,49 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'Статистика',
+    path: '/',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'Мои улья',
+    path: '/user',
+    icon: icon('ic_hives'),
+  },
+  
+  //  {
+    //  title: 'product',
+    //  path: '/products',
+    //  icon: icon('ic_cart'),
+  //  },
+  {
+    title: 'Пользователи',
+    path: '/user',
+    icon: icon('ic_user'),
+  },
+
+  {
+    title: 'Блог',
+    path: '/blog',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: icon('ic_lock'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: icon('ic_disabled'),
+  },
+];
+
+export default navConfig;
